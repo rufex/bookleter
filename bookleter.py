51bae2994c7f8e42ffc19c0e36765d1ee3a0db53
rufex <rufex.dev@gmail.com>
2022-07-30 11:03:53 +0200

    Re-arrange pages to the right order

--- bookleter.py
+++ bookleter.py
@@ -1,8 +1,39 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter, PageObject
 from pathlib import Path
+from collections import deque
 
 folder = Path(__file__).parent
 output = folder.joinpath('output.pdf')
+
+
+def reorder_pages(pages_nr: int):
+
+    if pages_nr % 4 == 0:
+        pairs = int(pages_nr/2)
+    else:
+        raise Exception('The number of pages should be a divisible by 4.')
+
+    pages = deque()
+
+    for i in range(1,pages_nr+1):
+        pages.append(i)
+
+    ordered_list = []
+
+    for i in range(0,pairs):
+        pair = []
+        # Pair
+        if i % 2 == 0:
+            pair.append(pages.pop())
+            pair.append(pages.popleft())
+        # Odd
+        else:
+            pair.append(pages.popleft())
+            pair.append(pages.pop())
+        ordered_list.append(pair)
+    
+    return ordered_list
+
 
 def main():
     pdf_writer = PdfFileWriter()
@@ -10,6 +41,9 @@ def main():
 
     pages_amount = pdf_reader.getNumPages()
     print('Number of Pages (Original File):',pages_amount)
+
+    new_order = reorder_pages(pages_amount)
+    print('Order in which the pages are going to be included:', new_order)
 
     page_size = pdf_reader.getPage(0).mediaBox.upperRight
     print('Page Size (First Page):', page_size)
@@ -19,11 +53,15 @@ def main():
 
     page_count = 0
 
-    for i in range(0, pages_amount, 2):
+    for pair in new_order:
+
         page_count += 1
 
-        page_left = pdf_reader.getPage(i)
-        page_right = pdf_reader.getPage(i+1)
+        i_left = pair[0]-1
+        i_right = pair[1]-1
+
+        page_left = pdf_reader.getPage(i_left)
+        page_right = pdf_reader.getPage(i_right)
 
         # Create Blank Page where we are going to render two pages on top of it
         merged = PageObject.createBlankPage(width=page_size_w*2, height=page_size_h)
@@ -39,6 +77,7 @@ def main():
     with open(output, 'wb') as F:
         pdf_writer.write(F)
         print('Done. PDF generated')
+
 
 if __name__ == "__main__":
     main()
